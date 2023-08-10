a2fd6b2032074533e3573ebde88ece3ae9a74c93
Wikicrafter <giga.anchabadze@gtu.ge>
2023-08-10 17:44:49 +0400

    Display on Console

---
+++ script.js
@@ -0,0 +1,49 @@
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'Paper')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+        const computerSelection = getComputerChoice();
+
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you win the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game. Better luck next time!");
+    } else {
+        console.log("It's a tie! The game ends in a draw.");
+    }
+}
+
+game(); // Start the game
